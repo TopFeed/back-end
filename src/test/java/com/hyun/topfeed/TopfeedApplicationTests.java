5a9739f1163e7b38999a934a0d0fad385ec56a27
HyunTae Jo <hyuntae9912@naver.com>
2024-07-19 18:40:07 +0900

    오류 수정

--- src/test/java/com/hyun/topfeed/TopfeedApplicationTests.java
+++ src/test/java/com/hyun/topfeed/TopfeedApplicationTests.java
@@ -1,13 +1,11 @@
 package com.hyun.topfeed;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TopfeedApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
